e9658737141e795885214dd8a3b1c20fe6347bb3
Pedro Coelho <71984422+Luckycaat@users.noreply.github.com>
2022-01-23 23:49:59 -0300

    Update drop_table_test.py

--- drop_table_test.py
+++ drop_table_test.py
@@ -1,7 +1,7 @@
 import sqlite3 as conector
 
 # Abertura de conexão e aquisição de cursor
-conexao = conector.connect("./meu_banco.db")
+conexao = conector.connect("./projetos.db")
 cursor = conexao.cursor()
 
 # Execução de um comando: SELECT... CREATE ...
